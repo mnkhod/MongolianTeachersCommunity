1104010e09748436c12a261910c1d79fa1a0fece
mnkhod <eikyuu.hoshi@gmail.com>
2020-05-16 18:21:56 +0800

    Added Views and Routes

---
+++ TeachersCommunityProject/TeachersCommunityProject/MainWeb/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.index,name="home"),
+    path('news/',views.news_archive,name="news"),
+    path('laws/',views.laws_archive,name="laws"),
+    path('aboutUs/',views.about_us,name="aboutUs"),
+    path('contactUs/',views.contact_us,name="contactUs"),
+    path('news/<int:news_id>/',views.news_single,name="newsSingle"),
+    path('laws/<int:laws_id>/',views.laws_single,name="lawsSingle"),
+]
--- TeachersCommunityProject/TeachersCommunityProject/MainWeb/views.py
+++ TeachersCommunityProject/TeachersCommunityProject/MainWeb/views.py
@@ -1,3 +1,26 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+#Static
+def index(req):
+    return HttpResponse("Home")
+
+def about_us(req):
+    return HttpResponse("Bidnii Tuhai")
+
+def contact_us(req):
+    return HttpResponse("Holboo Barih")
+
+#Dynamic
+def news_archive(req):
+    return HttpResponse("Medee Archive")
+
+def news_single(req,news_id):
+    return HttpResponse("Medee Single")
+
+def laws_archive(req):
+    return HttpResponse("Huuli Archive")
+
+def laws_single(req,laws_id):
+    return HttpResponse("Huuli Single")
