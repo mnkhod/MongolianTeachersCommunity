b57e4c6294623014bad784bc18f0dc84ebafff29
mnkhod <eikyuu.hoshi@gmail.com>
2020-05-17 19:49:12 +0800

    Added Admin Model View Mongol Version Text

--- TeachersCommunityProject/TeachersCommunityProject/MainWeb/admin.py
+++ TeachersCommunityProject/TeachersCommunityProject/MainWeb/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from .models import Bagsh,Post,Comment,ContactUs
 
+class BagshAdmin(admin.ModelAdmin):
+    list_display = ('name','email')
+
 # Register your models here.
-admin.site.register(Bagsh)
+admin.site.register(Bagsh,BagshAdmin)
 admin.site.register(Post)
 admin.site.register(Comment)
 admin.site.register(ContactUs)
--- TeachersCommunityProject/TeachersCommunityProject/MainWeb/models.py
+++ TeachersCommunityProject/TeachersCommunityProject/MainWeb/models.py
@@ -3,14 +3,18 @@ from django.utils import timezone
 # Create your models here.
 
 class Bagsh(models.Model):
-    name=models.CharField(max_length=250)
-    email=models.EmailField()
-    profileImg=models.ImageField()
+    name=models.CharField(max_length=250,verbose_name="Нэр")
+    email=models.EmailField(verbose_name="Цахим Хаяг")
+    profileImg=models.ImageField(upload_to='teachers',verbose_name="Зураг")
     created = models.DateTimeField(auto_now_add=True, editable=False)
+
+    def __str__(self):
+        return self.name
+
     class Meta:
         ordering =('name',)
-        def __str__(self):
-            return self.name
+        verbose_name="Багш"
+        verbose_name_plural="Багш Нар"
 
 class Post(models.Model):
     title=models.CharField(max_length=250)
@@ -19,10 +23,12 @@ class Post(models.Model):
     body=models.TextField()
     created = models.DateTimeField(auto_now_add=True, editable=False)
     updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
     class Meta:
         ordering =('-created',)
-        def __str__(self):
-            return self.title
 
 class Comment(models.Model):
     name=models.CharField(max_length=250)
@@ -31,10 +37,11 @@ class Comment(models.Model):
     created=models.DateTimeField(auto_now_add=True,editable=False)
     post_id=models.ForeignKey(Post, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.name
+
     class Meta:
         ordering =('-created',)
-        def __str__(self):
-            return self.name
 
 class ContactUs(models.Model):
     fullName=models.CharField(max_length=254)
@@ -43,7 +50,8 @@ class ContactUs(models.Model):
     text=models.TextField()
     created = models.DateTimeField(auto_now_add=True, editable=False)
 
+    def __str__(self):
+        return self.fullName
+
     class Meta:
         ordering =('-created',)
-        def __str__(self):
-            return self.fullName
