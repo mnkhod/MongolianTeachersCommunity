a4cd9c94a52b65e5ce0dbf1093b366ee234355d3
mnkhod <eikyuu.hoshi@gmail.com>
2020-05-18 20:00:18 +0800

    Added Admin Default Panel Interface Translation To Mongolian

--- TeachersCommunityProject/TeachersCommunityProject/MainWeb/admin.py
+++ TeachersCommunityProject/TeachersCommunityProject/MainWeb/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.admin.sites import AdminSite as site
 from .models import Bagsh,News,Law,Comment,ContactUs
 
 class BagshAdmin(admin.ModelAdmin):
@@ -10,3 +11,16 @@ admin.site.register(News)
 admin.site.register(Law)
 admin.site.register(Comment)
 admin.site.register(ContactUs)
+
+# Overriding Default Admin Panel Interface Text Contents
+site.site_header = "Монголын Багш Нарын Үйлвэрчний Эвлэлийн Холбоо" #Django Administration
+site.site_title = "МБНҮЭХ" #Django Site Admin
+site.index_title = "Админы Хэсэг" # Site Administration
+
+# AdminSite.index_template
+# AdminSite.app_index_template
+# AdminSite.login_template
+# AdminSite.login_form
+# AdminSite.logout_template
+# AdminSite.password_change_template
+# AdminSite.password_change_done_template
