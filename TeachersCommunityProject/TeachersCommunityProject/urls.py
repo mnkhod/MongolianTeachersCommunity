b41427b9fb44a45d6cd5fc0d35222e1d2bb7fa41
mnkhod <eikyuu.hoshi@gmail.com>
2020-05-21 14:56:17 +0800

    Added Admin Panel Full Translation

--- TeachersCommunityProject/TeachersCommunityProject/urls.py
+++ TeachersCommunityProject/TeachersCommunityProject/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path , include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns as lang
 
 urlpatterns = [
     path('admin/', admin.site.urls),
